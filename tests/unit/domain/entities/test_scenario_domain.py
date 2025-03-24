94c176eafb51da03ff7749003584b3f4760e492d
Huang Chuan <river1440>
2025-03-24 13:41:45 +0800

    chore: revised scenario domain test

--- tests/unit/domain/entities/test_scenario_domain.py
+++ tests/unit/domain/entities/test_scenario_domain.py
@@ -167,21 +167,121 @@ class TestScenarioDomainCase:
         # Assert that the resource field in the association is correctly set
         assert getattr(assoc, resource_field) == resource_data[resource_field]
 
+    @pytest.mark.parametrize(resource_param["param"], resource_param["payload"])
+    def test_retrieve_resource_to_scenario_domain(
+        self,
+        default_scenario_domain,
+        resource_factory,
+        resource_attr,
+        resource_name,
+        resource_association_cls,
+        resource_field,
+        resource_data,
+        update_data,
+    ):
+        # Arrange: Generate the resource instance
+        resource_instance = (
+            resource_factory()
+        )  # Use the factory to generate the resource with the data
+
+        # Act: Add the resource to the scenario domain by creating and adding association
+        resource_association = resource_association_cls(
+            scenario=default_scenario_domain,
+            **{resource_name: resource_instance},
+            **resource_data,
+            created_at=datetime.now(timezone.utc),
+            updated_at=datetime.now(timezone.utc),
+        )
+        default_scenario_domain._add_association(resource_association)
+
         # Assert: Ensure association can be retrieved by resource instance
         retrieved_association = default_scenario_domain.get_association_by_resource(
             resource_instance
         )
         assert retrieved_association == resource_association
 
+    @pytest.mark.parametrize(resource_param["param"], resource_param["payload"])
+    def test_update_resource_to_scenario_domain(
+        self,
+        default_scenario_domain,
+        resource_factory,
+        resource_attr,
+        resource_name,
+        resource_association_cls,
+        resource_field,
+        resource_data,
+        update_data,
+    ):
+        # Arrange: Generate the resource instance
+        resource_instance = (
+            resource_factory()
+        )  # Use the factory to generate the resource with the data
+
+        # Act: Add the resource to the scenario domain by creating and adding association
+        resource_association = resource_association_cls(
+            scenario=default_scenario_domain,
+            **{resource_name: resource_instance},
+            **resource_data,
+            created_at=datetime.now(timezone.utc),
+            updated_at=datetime.now(timezone.utc),
+        )
+        default_scenario_domain._add_association(resource_association)
+        resource_collection = getattr(default_scenario_domain, resource_attr)
+
         # Act: Update the resource data in the association
         default_scenario_domain._update_association(resource_association, **update_data)
 
         # Assert: Ensure the resource data is updated correctly
-        updated_field_value = getattr(resource_collection[0], resource_field)
-        assert updated_field_value == update_data[resource_field]
+        updated_assoc = next(
+            (assoc for assoc in resource_collection if assoc == resource_association),
+            None,
+        )
+
+        # Assert that the updated association reflects the new field value
+        assert (
+            updated_assoc is not None
+        ), "The association was not found in the collection"
+
+        updated_field_value = getattr(updated_assoc, resource_field)
+        assert (
+            updated_field_value == update_data[resource_field]
+        ), f"Expected {resource_field} to be {update_data[resource_field]}, but got {updated_field_value}"
+
+    @pytest.mark.parametrize(resource_param["param"], resource_param["payload"])
+    def test_delete_resource_to_scenario_domain(
+        self,
+        default_scenario_domain,
+        resource_factory,
+        resource_attr,
+        resource_name,
+        resource_association_cls,
+        resource_field,
+        resource_data,
+        update_data,
+    ):
+        # Arrange: Generate the resource instance
+        resource_instance = (
+            resource_factory()
+        )  # Use the factory to generate the resource with the data
+
+        # Act: Add the resource to the scenario domain by creating and adding association
+        resource_association = resource_association_cls(
+            scenario=default_scenario_domain,
+            **{resource_name: resource_instance},
+            **resource_data,
+            created_at=datetime.now(timezone.utc),
+            updated_at=datetime.now(timezone.utc),
+        )
+        default_scenario_domain._add_association(resource_association)
+
+        # Assert: Ensure the resource is removed from the scenario domain
+        origin_length = len(getattr(default_scenario_domain, resource_attr))
 
         # Act: Delete the resource association from the scenario
         default_scenario_domain._delete_association(resource_association)
 
         # Assert: Ensure the resource is removed from the scenario domain
-        assert len(getattr(default_scenario_domain, resource_attr)) == 0
+        updated_length = len(getattr(default_scenario_domain, resource_attr))
+        assert updated_length == (
+            origin_length - 1
+        ), f"Expected collection length to decrease by 1, but got {updated_length}"
